9786004d738431d42fc4060f3ab26ceef59ec72a
jmauricio2019 <54713911+jmauricio2019@users.noreply.github.com>
2020-06-14 14:29:35 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="UTF-8"/>
+	<title>Cátalago OnLine do Zé</title>   
+	<link rel="stylesheet" type="text/css"href="_css/estilo.css"/>	
+	<link rel="stylesheet" href="_css/fotos.css">
+<script type="text/javascript" src="http://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=X05EnC_a7noUA9jT9o9OSg0vuTwhpfJGQ4Mmc4xFl3-4Hgs0mcqdIdX6UjoTC5fEmoclS02wg-jGdMfiTDYQ8Q" charset="UTF-8"></script></head>
+<script language="javascript" src="_javascript/funcoes.js"></script>
+<body>
+<div id="interface">
+
+	<header id="cabecalho">
+	<hgroup>
+	<h1>Cátalago OnLine</h1>
+        <h2>Utilidades em Geral</h2>
+	</hgroup>
+	 
+	<nav id="menu">
+	<h1>Menu Principal</h1>
+	<ul type="1" start="1">	
+		<li><a href="index.php">Home</a></li>
+		<li><a href="specs.html">Cozinha</a></li>
+		<li ><a href="fotos.html">Decoração</a></li>
+		<li><a href="multimidia.html">Diversos</a></li>
+		<li><a href="fale-conosco.html">Vendas</a></li>
+	</ul>
+	</nav>
+</header>
+<section id="corpo-full">
+    
+	<article id="noticia-principal">
+		<header id="cabecalho-artigo">
+			<hgroup>
+				<h3>Cátalago OnLine > Home</h3>
+			</hgroup>
+		</header>
+		
+<p> </p>
+<ul id="album-fotos">
+	<li id="foto01"><span>Teste         Cód.: R001</span></li>
+	<li id="foto02"><span>Teste         Cód.: R002</span></li>
+	<li id="foto03"><span>Teste         Cód.: R003</span></li>
+	<li id="foto04"><span>Teste         Cód.: R004</span></li>
+	<li id="foto05"><span>Teste         Cód.: R005</span></li>
+	<li id="foto06"><span>Teste         Cód.: R006</span></li>
+</ul>
+
+</section>
+<footer id="rodape">
+    
+AQUI ENTRA WATHS PARA CONTATO E VENDAS JUNTAMENTE COM QRCODE
+
+</footer>
+</div>
+</body>
+</html>
